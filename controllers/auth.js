20f4d99daf46f2c4b56981a3171c05c7bc70bfbf
leonardog9823 <guanuco43@gmail.com>
2022-05-12 02:23:56 -0300

    vista login

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,22 +8,36 @@ const signInVista = ( req = request, res = response ) => {
     res.status(200).render("auth/sign-in");
 }
 
+const loginVista = ( req = request, res = response ) => {
+    res.status(200).render("auth/login");
+}
+
 const createUser = async ( req = request, res = response ) => {
 
     const { username, password, email } = req.body;
 
     const salt = bcrypt.genSaltSync(10);
     const hashPassword = bcrypt.hashSync(password, salt);
-
-    console.log(hashPassword);
     
-    const usuario = new Usuario({ username, password, email });
+    const usuario = new Usuario({ username, password: hashPassword, email });
     await usuario.save();
+
+    res.redirect("../home");
+}
+
+const loginUser = async ( req = request, res = response ) => {
+    const { email, password } = req.body;
+
+    const getPassword = await Usuario.findOne({ email });
+    const comparePassword = bcrypt.compareSync( password, getPassword.password );
+
 
     res.redirect("../home");
 }
 
 module.exports = {
     signInVista,
-    createUser
+    createUser,
+    loginVista,
+    loginUser
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,16 @@
 const { Router } = require("express");
-const { signInVista, createUser } = require("../controllers/auth");
+const { signInVista, createUser, loginVista, loginUser } = require("../controllers/auth");
 
 const router = Router();
 
-router.get( "/sign-in", signInVista );
+router.get( "/login", loginVista );
+router.post( "/login", loginUser );
 
+router.get( "/sign-in", signInVista );
 router.post( "/sign-in-create", createUser );
 
 router.get( "../home", ( req, res ) => {
-    res.render("home")
+    res.render("home");
 })
 
 module.exports = router;
