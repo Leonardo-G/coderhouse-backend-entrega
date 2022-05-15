2af28003db9bc12b4f31b0fad675a217a56821e4
leonardog9823 <guanuco43@gmail.com>
2022-05-15 16:09:52 -0300

    vista subCategories

--- db/config.js
+++ db/config.js
@@ -4,6 +4,9 @@ const connectMongo = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL_DATABASE);
         console.log("Base a MongoDB conectado");
+        mongoose.connection.on('error', function(error) {
+            console.error('Database connection error:', error);
+          });
     } catch (error) {
         console.log("Error al conectar la base a Mongo", error);
     }
--- public/js/subCategories.js
+++ public/js/subCategories.js
@@ -2,17 +2,45 @@ const url = window.location.href;
 const urlSplit = url.split("/");
 const category = urlSplit[urlSplit.length - 1];
 
+var subCategories;
+
 document.addEventListener("DOMContentLoaded", () => {
     getSubCategory();
 })
 
 const getSubCategory = async () => {
+
+    const cajaSubCategorie = document.querySelector(".subcategories");
+
     try {
         const res = await fetch(`http://localhost:8000/api/categories/${category}/subcategories`, {
             method: "GET",
         })
         const resp = await res.json();
-        console.log(resp);
+
+        resp.forEach( subCategory => {
+            cajaSubCategorie.innerHTML += `
+                <a href='/category/${ subCategory.category }' class="subcategories__card">
+                    <div>
+                        <img src=${ subCategory.imgSubCategory } alt=${ subCategory }/>
+                    </div>
+                    <div class="card--title">
+                        <p>${ subCategory.subCategory }</p>
+                    </div>
+                </a>
+            `
+        })
+        // const htmlCardSubCategorie = `
+        //     <div class="subcategories__card">
+        //         <div> 
+        //             img(src=`${enlace}/img/images.jpg`, alt="Logo Mercado Libre")
+        //         </div>
+        //         <div class="card--title">
+        //             <p> Electricas </p>
+        //         </div>
+        //     </div>
+        //         `
+                console.log(resp);
     } catch (error) {
         console.log(error);
     }
