3a1c1acdddae8f3d29a91fa2833590eeff2c646b
leonardog9823 <guanuco43@gmail.com>
2022-05-15 21:18:51 -0300

    vista /products

--- public/js/products.js
+++ public/js/products.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", () => {
     getSubCategory();
 })
@@ -6,13 +5,35 @@ document.addEventListener("DOMContentLoaded", () => {
 const getSubCategory = async () => {
     const pathSubCategory = window.location.href.split("/");
     const subCategory = pathSubCategory[pathSubCategory.length - 1]; 
-console.log(pathSubCategory)
+    const divBoxProduct = document.querySelector(".product");
     try {
         const res = await fetch(`http://localhost:8000/api/products/subcategory/${subCategory}`, {
             method: "GET",
         })
         const resp = await res.json();
-        console.log(resp)
+        resp.products.forEach( product => {
+            divBoxProduct.innerHTML += `
+                <div class="product__card">
+                    <div class="card--img">
+                        <a href="/product/${ product._id }">
+                            <img src=${ product.imgProduct[0] } alt=${ product.title }/>
+                        </a>
+                    </div>
+                    <div class="card--info">
+                        <a href="/product/${ product._id }">
+                            <h2 class="title">
+                                ${ product.title }
+                            </h2>
+                        </a>
+                        <a href="/product/${ product._id }">
+                            <p class="price">
+                                $ ${ Number(product.price).toLocaleString('es-AR') }
+                            </p>
+                        </a>
+                    </div>
+                </div>
+            ` 
+        })
     } catch (error) {
         console.log(error);
     }
