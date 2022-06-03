b90ad20b5335f0e53d2307d56ba1205936f3a884
leonardog9823 <guanuco43@gmail.com>
2022-06-03 16:35:55 -0300

    estilos product

--- public/js/product.js
+++ public/js/product.js
@@ -6,6 +6,7 @@ const getSubCategory = async () => {
     const pathProduct = window.location.href.split("/");
     const product = pathProduct[pathProduct.length - 1]; 
     const divBoxProduct = document.querySelector(".product");
+    const divPayment = document.querySelector(".payment");
     try {
         const res = await fetch(`http://localhost:8000/api/products/product/${product}`, {
             method: "GET",
@@ -13,6 +14,7 @@ const getSubCategory = async () => {
         const resp = await res.json();
         console.log(resp);
         const characteristics = resp.characteristics;
+        const noBlock = "<p style={display: none}></p>"
         divBoxProduct.innerHTML += `
             <div class="product__galery">
                 <img src=${ resp.imgProduct[0] } />
@@ -26,7 +28,7 @@ const getSubCategory = async () => {
                     <i class="fa-regular fa-heart icon--fav"></i>
                 </div>
                 <h1>${ resp.title }</h1>
-                ${ resp.typePrice.old !== 0 ? `<p class="price--old">$ ${resp.typePrice.old.toLocaleString("es-AR")} </p>` : "<p style={display: none}></p>"}
+                ${ resp.typePrice.old !== 0 ? `<p class="price--old">$ ${resp.typePrice.old.toLocaleString("es-AR")} </p>` : noBlock}
                 
                 <p class="price">$ ${ resp.typePrice.current.toLocaleString("es-AR") } <span class="descuento"> ${ resp.typePrice.old !== 0 && Math.round((resp.typePrice.old - resp.typePrice.current) / (resp.typePrice.old / 100))  }% OFF</span></p>
                 <span class="cuota">en 12x $ ${ (resp.typePrice.current / 12).toLocaleString("es-AR", { maximumFractionDigits: "2", minimumFractionDigits: "2" }) } sin interés</span>
@@ -38,8 +40,60 @@ const getSubCategory = async () => {
                     </ul>
                 </div>
             </div>
-        `
+        `;
+        divPayment.innerHTML += `
+            <div class="payment__delivery">
+                <i class="fa-solid fa-truck-fast icon--thunk"></i>
+                <p>Llega gratis <span>${ dayDelivery(resp.delivery) === "mañana" ? "mañana" : `el ${dayDelivery(resp.delivery)}`}</span></p>    
+            </div>
+            ${ resp.stock > 0 ? "<p class='stock_true'>Stock disponible</p>" : noBlock }   
+            ${
+                resp.stock > 0
+                ? 
+                    `<div class="payment__select">
+                        <p>Cantidad: </p>
+                        <input type="number" name="cantidad" value="1"/>
+                        <p>unidad/es</p>
+                        <p class="stock">(${ resp.stock } disponibles)</p>
+                    </div>`
+                    
+                : noBlock
+            }
+            <button class="payment__button button--buy">Comprar ahora</button>
+            <button class="payment__button button--add">Agregar al carrito</button>
+            <div class="payment_info">
+                <div class="info--p">
+                    <i class="fa-solid fa-share"></i>
+                    <p><span>Devolución gratis.</span> Tenés 30 dias desde que lo recibís</p>
+                </div>
+                <div class="info--p">
+                    <i class="fa-regular fa-circle-check"></i>
+                    <p><span>Compra protegida.</span> recibrí el producto que esperabas o e devolvemos tu dinero.</p>
+                </div>
+                <div class="info--p">
+                    <i class="fa-solid fa-award"></i>
+                    <p>12 meses de garantía de fábrica.</p>
+                </div>
+            </div>
+        `;
+
+        document.title = resp.title;
     } catch (error) {
         console.log(error);
     }
+}
+
+//POSIBLE DTO
+const dayDelivery = (day) => {
+    let deliveryDay;
+    const dayNext2 = new Date().setDate(new Date().getDate() + 3);
+
+    if( day === 1){
+        deliveryDay = new Date(dayNext2).toLocaleDateString("es-AR", {weekday: 'long'});
+    }
+    if( day === 2 ){
+        deliveryDay = "mañana"
+    }
+    
+    return deliveryDay;
 }
